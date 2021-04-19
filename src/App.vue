bf68ffd5de730b3ce9e8cc2258f416d90967962b
Amanda Wallin <amandawallin@live.se>
2021-04-19 15:36:39 +0200

    feat: added meta tags

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,11 @@
 export default {
   name: 'App',
   metaInfo: {
-    title: 'Clowns Without Borders'
+    meta: [
+      { name: 'title', content: 'Creative Arts for Peace and Equality' },
+      { name: 'description', content: 'Creative Arts for Peace and Equality is a project started by Clowns Without Borders Sweden, financed by the Folke Bernadotte Academy.' },
+      { property: 'og:image', content: `${process.env.BASE_URL}share.png`}
+    ]
   }
 }
 </script>
